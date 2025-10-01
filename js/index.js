2b9c1ef80ccbf25ddd2c0d8bad9ea65ced483e2c
Beatriz Santos <bsantos@nvidia.com>
2025-09-30 19:16:49 -0700

    form and functionality added

---
+++ js/index.js
@@ -0,0 +1,39 @@
+// Handle Message Form Submit
+const messageForm = document.querySelector('form[name="leave_message"]');
+
+messageForm.addEventListener('submit', function(event) {
+    // Prevent the default form submission
+    event.preventDefault();
+    
+    // Get the values from the form fields
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+    
+    // Log the three variables to the console
+    console.log('Name:', usersName);
+    console.log('Email:', usersEmail);
+    console.log('Message:', usersMessage);
+    
+    // Display messages in list
+    const messageSection = document.querySelector('#messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    
+    newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a><span>${usersMessage}</span>`;
+    
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+    
+    removeButton.addEventListener('click', function(event) {
+        const entry = event.target.parentNode;
+        entry.remove();
+    });
+    
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    
+    // Clear the form
+    messageForm.reset();
+});
